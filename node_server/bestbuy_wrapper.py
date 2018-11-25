9cd9bb05adfc50b52ca5177b9232e4d699a3ce9d
matt <stroiamatei@gmail.com>
2018-11-25 08:02:44 +0200

    mie rusine

---
+++ node_server/bestbuy_wrapper.py
@@ -0,0 +1,52 @@
+import requests
+from urllib.parse import urlencode
+
+
+class BestBuyProduct:
+    def __init__(self, response):
+        self.price = response['regularPrice']
+        d = {detail['name']: detail['value'] for detail in response['details']}
+        self.name = d['Product Name'].replace('Unlocked', '').replace('with', '').replace('LTE', '').replace('Cell Phone', '').replace('Memory', '').replace('()', '')
+        self.manufacturer = d['Device Manufacturer']
+        self.screen_size = float(d['Screen Size'][0:-7])
+        try:
+            self.back_camera = float(d['Rear-Facing Camera'][0:-11])
+        except KeyError:
+            self.back_camera = 5
+        try:
+            self.front_camera = float(d['Front-Facing Camera'][0:-11])
+        except KeyError:
+            self.front_camera = 5
+        try:
+            self.battery = float(d['Maximum Usage Time'][0:-6])
+        except KeyError:
+            self.battery = 25
+        try:
+            self.ram = float(d['Phone Memory (RAM)'][0:-10])
+        except KeyError:
+            self.ram = 2.5
+
+    def __eq__(self, other):
+        if isinstance(other, BestBuyProduct):
+            return self.name == other.name
+        return False
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+class BestBuy:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_products(self, q):
+        ret = []
+        for page in range(1, 10):
+            response = requests.get('https://api.bestbuy.com/v1/products({})'.format(q),
+                                    params={'show': 'details,regularPrice', 'apiKey': self.api_key, 'format': 'json',
+                                            'sort': 'customerReviewAverage.desc', 'pageSize': 100, 'page': page})
+            ret.extend([BestBuyProduct(p) for p in response.json()['products']])
+        return ret
